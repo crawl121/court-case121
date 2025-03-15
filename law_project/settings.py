7dd05773125f4539ab138097f3aa75d27bdd0df3
Shubham <165379388+crawl121@users.noreply.github.com>
2025-03-15 18:33:00 +0530

    Update settings.py

--- law_project/settings.py
+++ law_project/settings.py
@@ -76,23 +76,23 @@ WSGI_APPLICATION = 'law_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'railway',
-        'USER': 'postgres',
-        'PASSWORD': 'lZwWZRVxGaczkssHZnVbEuclmSCnhFCX',
-        'HOST': 'monorail.proxy.rlwy.net',
-        'PORT': '22102',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'railway',
+#         'USER': 'postgres',
+#         'PASSWORD': 'lZwWZRVxGaczkssHZnVbEuclmSCnhFCX',
+#         'HOST': 'monorail.proxy.rlwy.net',
+#         'PORT': '22102',
+#     }
+# }
 
 
 # Password validation
